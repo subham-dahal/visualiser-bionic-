98586dea76909a6751490cd39e8820861f1f1235
evillord982 <ashunterminator@gmail.com>
2026-08-25 21:34:05 +1000

    restructure repo, convert to react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Bionic Visualiser</h1>
+      </header>
+      <main className="canvas-region">
+        {/* Visualizer Canvas*/}
+      </main>
+      <aside className="detail-region">
+        {/* Item Detail Panel */}
+      </aside>
+    </div>
+  )
+}
+
+export default App
